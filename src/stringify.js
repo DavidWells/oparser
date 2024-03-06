82d7ab2a3a0957b2586d05d932fe6d2089d65187
DavidWells <hello@davidwells.io>
2024-03-05 22:17:25 -0800

    feat: make stringifier handle deep objects

--- src/stringify.js
+++ src/stringify.js
@@ -1,3 +1,4 @@
+
 /**
  * Turn object into options string
  * @param {Record<string, any>} obj 
@@ -7,6 +8,12 @@
 function stringify(obj, opts = {}) {
   if (typeof obj !== 'object') return ''
   const joiner = opts.joiner || '='
+  if (typeof opts.asJs === 'undefined') {
+    opts.asJs = true
+  }
+  if (typeof opts.compressed === 'undefined') {
+    opts.compressed = false
+  }
   const attrs = Object.entries(obj)
     // filter out non-serializable values
     .filter(([attr, val]) => {
@@ -14,44 +21,101 @@ function stringify(obj, opts = {}) {
       return type !== 'undefined' && val !== null && type !== 'function'
     })
     .map(([attr, val], i) => {
+      // console.log(`start val ${attr}`, val)
       let value = format(val, opts)
+      // console.log(`check 1 ${attr}`, value)
+
       /* return array of items */
       if (Array.isArray(value)) {
+        // console.log('val', val)
         const mapped = format(val, opts)
-        return `${attr}${joiner}{[${mapped.join(', ')}]}`
+        // console.log('mapped', mapped)
+        const joined = mapped.map((x) => {
+          if (typeof x === 'object') {
+            return formatObject(x, opts)
+          }
+          return x
+        }).join(', ')
+        // console.log('joined', joined)
+        return `${attr}${joiner}{[${joined}]}`
       }
       if (typeof val === 'object') {
-        return `${attr}${joiner}{${stringifyWithSpaces(val)}}`
+        const formatted = formatObject(val, opts)
+        return `${attr}${joiner}{${formatted}}`
       }
       /* wrap in brackets for multiline values */
       if (typeof val === 'string' && value.indexOf('\n') > -1) {
         value = `{${value.replace(/^"|"$/g, '`')}}`
       }
       return `${attr}${joiner}${value}`
-    }).join(opts.separator || ' ')
+    }).join(opts.separator || '\n')
   return attrs
 }
 
-function format(val, opts, insideArray) {
+function formatObject(obj, opts) {
+  let cleanObj = ''
+  if (opts.compressed) {
+    cleanObj = JSON.stringify(obj)
+    /*
+    console.log('single line JSON no spaces', cleanObj)
+    /** */
+  } else if (opts.singleLineValues) {
+    cleanObj = stringifyWithSpaces(obj)
+    /*  
+    console.log('single line clean JSON', cleanObj)
+    /** */
+
+  } else if (opts.expanded) {
+    cleanObj = JSON.stringify(obj, null, 2)
+    /*
+    console.log('Deep JSON with newlines', cleanObj)
+    /** */
+  } else {
+    cleanObj = prettier(obj, { bracketSpacing: true })
+    /*
+    console.log('prettier obj', cleanObj)
+    /** */
+  }
+  return opts.asJs ? jsonToJsObject(cleanObj) : cleanObj
+}
+
+function jsonToJsObjectTwo(jsonStr = '') {
+  return jsonStr
+    .replace(/^([\t ]*)("([^\\"]*|\\.)*")([\t ]*):([\t ]*)/gm, '$1$3: ')
+    .replace(/^([\t ]*){?[\t ]*("([^\\"]*|\\.)*")([\t ]*):([\t ]*)/gm, '$1{ $3: ')
+}
+
+function jsonToJsObject(str){
+  arr = str.match(/".*?":/g);
+  for (var i = 0; i < arr.length; i++) {
+    str = str.replace(arr[i], arr[i].replace(/"/g, ''))
+  }
+  return str;
+}
+
+function format(val, opts, insideArray, insideObject) {
   // console.log(typeof val, val)
   const type = typeof val
   if (type === 'undefined' || val === null) {
     return val
   }
-  if (type === 'string') {
+  if (type === 'string' && !insideArray && !insideObject) {
     return ensureQuote(val)
   }
   if (Array.isArray(val)) {
-    return val.map((v) => format(v, opts, 'array'))
+    return val.map((v) => {
+      // console.log('v', v)
+      return format(v, opts, insideArray, insideObject)
+    })
   }
   if (typeof val === 'object') {
     const obj = {}
     const keys = Object.keys(val)
     for (let i = 0; i < keys.length; i++) {
-      obj[keys[i]] = format(val[keys[i]], opts)
+      obj[keys[i]] = format(val[keys[i]], opts, null, true)
     }
-    const multiline = opts.separator === '\n' ? 2 : 0
-    return (insideArray) ? `${JSON.stringify(obj, null, multiline).replace(/"\\"/g, '"').replace(/\\""/g, '"')}` : obj
+    // console.log('ret obj', obj)
+    return obj
   }
   return val
 }
@@ -68,6 +132,91 @@ function stringifyWithSpaces(obj) {
   // remove spaces between array-brackets and first/last items
 	result = result.replace(/\[ /g, "[").replace(/ \]/g, "]")
 	return result
+}
+
+const stringOrChar = /("(?:[^\\"]|\\.)*")|[:,]/g
+
+// https://github.com/lydell/json-stringify-pretty-compact/blob/main/index.js
+function prettier(passedObj, options = {}) {
+  const indent = JSON.stringify([1], undefined, options.indent === undefined ? 2 : options.indent).slice(2, -3)
+  const maxLength = indent === "" ? Infinity : options.maxLength === undefined ? 80 : options.maxLength
+  let { replacer } = options
+
+  return (function _stringify(obj, currentIndent, reserved) {
+    if (obj && typeof obj.toJSON === "function") {
+      obj = obj.toJSON();
+    }
+
+    const string = JSON.stringify(obj, replacer)
+
+    if (string === undefined) {
+      return string;
+    }
+
+    const length = maxLength - currentIndent.length - reserved;
+
+    if (string.length <= length) {
+      const prettified = string.replace(stringOrChar, (match, stringLiteral) => {
+        return stringLiteral || `${match} `
+      })
+      // console.log('prettified', prettified)
+      if (prettified.length <= length) {
+        //return prettified
+        return options.bracketSpacing ? bracketSpacing(prettified) : prettified
+      }
+    }
+
+    if (replacer != null) {
+      obj = JSON.parse(string)
+      replacer = undefined
+    }
+
+    if (typeof obj === "object" && obj !== null) {
+      const nextIndent = currentIndent + indent
+      const items = [];
+      let index = 0
+      let start
+      let end
+
+      if (Array.isArray(obj)) {
+        start = "[";
+        end = "]";
+        const { length } = obj
+        for (; index < length; index++) {
+          items.push(_stringify(obj[index], nextIndent, index === length - 1 ? 0 : 1) || "null")
+        }
+      } else {
+        start = "{"
+        end = "}"
+        const keys = Object.keys(obj)
+        const { length } = keys
+        for (; index < length; index++) {
+          const key = keys[index]
+          const keyPart = `${JSON.stringify(key)}: `
+          const value = _stringify(obj[key], nextIndent, keyPart.length + (index === length - 1 ? 0 : 1))
+          if (value !== undefined) {
+            items.push(keyPart + value)
+          }
+        }
+      }
+
+      if (items.length > 0) {
+        return [start, indent + items.join(`,\n${nextIndent}`), end].join(`\n${currentIndent}`)
+      }
+    }
+
+    return string;
+  })(passedObj, "", 0)
+}
+
+function bracketSpacing(str) {
+  return str
+    .replace(/^\[{/, '[{ ')
+    .replace(/}\]$/, ' }]')
+    .replace(/^{\s*/, '{ ')
+    .replace(/\s*}$/, ' }')
+    .replace(/^\[([^{])\s*/, '[ $1')
+    .replace(/\s*([^}])\]$/, '$1 ]')
 }
 
 /**
--- src/stringify.test.js
+++ src/stringify.test.js
@@ -1,19 +1,26 @@
 const { test } = require('uvu') 
 const assert = require('uvu/assert')
 const { stringify } = require('./stringify')
+const { parse } = require('./index')
 
 test('stringify multiline values with `', t => {
-  const x = {
-    text: `multi
+  const multiLineTextValue = `multi
 
 line
 
-text`,
+text`
+  const x = {
+    text: multiLineTextValue,
     author: 'Author'
   }
-  const str = stringify(x)
-  // console.log('x', str)
-  assert.is(str, `text={\`multi
+  const stringValue = stringify(x, { separator: ' ' })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(parsedValue, x)
+  assert.is(stringValue, `text={\`multi
 
 line
 
@@ -21,7 +28,12 @@ text\`} author="Author"`)
 })
 
 test('stringify', () => {
-  const props = {
+  const trimmedItems = {
+    func: () => {},
+    nullish: null,
+    undef: undefined
+  }
+  const object = {
     text: 'hello',
     boolean: true,
     array: ['hi', 'there', true],
@@ -29,27 +41,45 @@ test('stringify', () => {
       cool: true,
       nice: 'awesome'
     },
-    func: () => {},
-    nullish: null,
-    undef: undefined
   }
 
-  const optsString = stringify(props)
-  assert.equal(optsString, 'text="hello" boolean=true array={["hi", "there", true]} object={{"cool": true, "nice": "awesome"}}')
+  const inputObject = {
+    ...object,
+    ...trimmedItems
+  }
 
-  const optsStringTwo = stringify(props, { separator: '\n' })
-  // console.log('optsStringTwo', optsStringTwo)
-  assert.equal(optsStringTwo, `
+  const stringValue = stringify(inputObject, { 
+    separator: ' ', 
+    // asJs: false,
+  })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(stringValue, 'text="hello" boolean=true array={["hi", "there", true]} object={{ cool: true, nice: "awesome" }}')
+
+
+  assert.equal(parsedValue, object)
+  const stringValueTwo = stringify(object, { separator: '\n' })
+  const parsedValueTwo = parse(stringValue)
+
+  /*
+  console.log('stringValueTwo', stringValueTwo)
+  console.log('parsedValueTwo', parsedValueTwo)
+  /** */
+  assert.equal(parsedValueTwo, object)
+  assert.equal(stringValueTwo, `
 text="hello"
 boolean=true
 array={["hi", "there", true]}
-object={{"cool": true, "nice": "awesome"}}
+object={{ cool: true, nice: "awesome" }}
 `.trim())
 })
 
 
 test('stringify deep', () => {
-  const props = {
+  const object = {
     "okay": "cool",
     "components": [
       {
@@ -70,28 +100,160 @@ test('stringify deep', () => {
     ]
   }
 
-  const optsStringTwo = stringify(props, { separator: '\n' })
-  // console.log('optsStringTwo', optsStringTwo)
-  assert.equal(optsStringTwo, `
+  const stringValue = stringify(object, { separator: '\n' })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(parsedValue, object)
+  assert.equal(stringValue, `
 okay="cool"
 components={[{
-  "type": "fullimage",
-  "height": 400,
-  "heading": "Heading here...",
-  "component": "fullimage",
-  "subheading": "ccccccc"
+  type: "fullimage",
+  height: 400,
+  heading: "Heading here...",
+  component: "fullimage",
+  subheading: "ccccccc"
 }, {
-  "type": "nice",
-  "height": 400,
-  "heading": "rad here...",
-  "component": "fullimage",
-  "subheading": "ccccccc",
-  "array": [
-    "hi",
-    "there"
+  type: "nice",
+  height: 400,
+  heading: "rad here...",
+  component: "fullimage",
+  subheading: "ccccccc",
+  array: [ "hi", "there" ]
+}]}
+`.trim())
+})
+
+test('stringify deep two', () => {
+  const object = {
+    "components": [
+      {
+        "type": "columns",
+        "number": "1",
+        "box": false,
+        "columns": [
+          {
+            "content": "nicex",
+          },
+        ]
+      }
+    ]
+  }
+
+  const stringValue = stringify(object, { separator: '\n' })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(parsedValue, object)
+  assert.equal(stringValue, `
+components={[{
+  type: "columns",
+  number: "1",
+  box: false,
+  columns: [{ content: "nicex" }]
+}]}
+`.trim())
+})
+
+
+test('stringify deep three', () => {
+  const object = {
+    "components": [
+      {
+        "type": "columns",
+        "number": "1",
+        "box": false,
+        "columns": [
+          {
+            "content": "nicex",
+            "array": ["hi", "there"],
+            foo: {
+              "type": "fullimage",
+              "height": 400,
+              "heading": "Heading here...",
+              "component": "fullimage",
+              "subheading": "ccccccc"
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+  const stringValue = stringify(object, { separator: '\n' })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(parsedValue, object)
+  assert.equal(stringValue, `
+components={[{
+  type: "columns",
+  number: "1",
+  box: false,
+  columns: [
+    {
+      content: "nicex",
+      array: [ "hi", "there" ],
+      foo: {
+        type: "fullimage",
+        height: 400,
+        heading: "Heading here...",
+        component: "fullimage",
+        subheading: "ccccccc"
+      }
+    }
   ]
 }]}
 `.trim())
+})
+
+
+
+test('stringify simple', () => {
+  const object = {
+    whatever: 'hi',
+    yo: 'hi',
+    type: "columns",
+    number: "1",
+    box: false,
+    funky: {
+      bunch: {
+        and: {
+          deep: {
+            value: 'cool'
+          }
+        }
+      }
+    }
+  }
+
+  const stringValue = stringify(object, { 
+    separator: '\n',
+    prettier: true,
+    // singleLineValues: true,
+  })
+  const parsedValue = parse(stringValue)
+  /*
+  console.log('stringValue', stringValue)
+  console.log('parsedValue', parsedValue)
+  /** */
+  assert.equal(stringValue, `
+whatever="hi"
+yo="hi"
+type="columns"
+number="1"
+box=false
+funky={{ bunch: {and: {deep: {value: "cool"}}} }}
+`.trim())
+
+
+  assert.equal(parsedValue, object)
 })
 
 
